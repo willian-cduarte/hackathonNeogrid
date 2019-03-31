7d375945c9580ea6327aa1ccf9008467806809b5
DaviWollmann <davi.wollmann.s@gmail.com>
2019-03-31 00:11:38 -0300

    alteracoes recomendadas pelo editor na classe cliente.model.ts
    alteracoes recomendadas pelo editor na classe cliente.model.ts

--- src/app/util/cliente.model.ts
+++ src/app/util/cliente.model.ts
@@ -1,12 +1,11 @@
-
 export class Cliente {
-  private idCliente: Number
-  private nome : string
-  private cnpj : string
-  private atendimentoTelefone : boolean
-  private cliente : boolean
-  private email : string
-  private grauOrganizacao : number
-  private contraSenha : string
-  private endereco : string
+  private idCliente: number;
+  private nome: string;
+  private cnpj: string;
+  private atendimentoTelefone: boolean;
+  private cliente: boolean;
+  private email: string;
+  private grauOrganizacao: number;
+  private contraSenha: string;
+  private endereco: string;
 }
