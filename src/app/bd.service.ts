dc0256513dbb2f209f76f178498425430829163f
Willian Cardoso Duarte <crds.willian@gmail.com>
2019-03-31 04:30:46 -0300

    home analista

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -29,6 +29,8 @@ export class BdService {
     }
 
     public consultaTicketsSuporte(): Promise<Ticket[]>{
+
+            let ticketArray: Ticket[] = [];
        
             return new Promise((resolve, reject) => {
 
@@ -42,10 +44,18 @@ export class BdService {
                             let ticket: Ticket = childSnapshot.val();
     
                             ticket.key = childSnapshot.key;
-    
+                           
                             ticketsList.push(ticket);
                         });
-                        resolve(ticketsList);
+                        console.log(ticketsList)
+
+                        ticketsList.forEach((ticket: any) => {
+                            console.log('ddd', ticket)
+
+                            ticketArray.push(ticket.ticketInsertTeste);
+                        })
+                        console.log(ticketArray)
+                        resolve(ticketArray);
                     });
             });
     }
