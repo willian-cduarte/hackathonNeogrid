e7ce3b36f98fbbcc3b6d78a8acf1d9715a5eab76
Willian Cardoso Duarte <crds.willian@gmail.com>
2019-03-30 23:03:19 -0300

    Classe cliente.ts criada

---
+++ src/app/util/cliente.model.ts
@@ -0,0 +1,12 @@
+
+export class Cliente {
+  private idCliente: Number
+  private nome : string
+  private cnpj : string
+  private atendimentoTelefone : boolean
+  private cliente : boolean
+  private email : string
+  private grauOrganizacao : number
+  private contraSenha : string
+  private endereco : string
+}
